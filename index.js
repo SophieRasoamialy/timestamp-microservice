ea94c6523fd6c4ae3aa84d236534de194f8ff918
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-02-26 17:54:52 +0000

    feat(api): Add handling for timestamp requests

--- index.js
+++ index.js
@@ -13,20 +13,30 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+
+app.get('/api/:date?', (req, res) => {
+  const dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    const timestamp = parseInt(dateString);
+    if (isNaN(timestamp)) {
+      date = new Date(dateString);
+    } else {
+      date = new Date(timestamp);
+    }
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
